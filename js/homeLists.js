0516208766a9403a74d2d50b4cfb9335966b30b5
Christian Peraza <christianperaza2000@gmail.com>
2024-07-24 23:17:59 -0300

    Upcoming and Top Rated Modal Done

--- js/homeLists.js
+++ js/homeLists.js
@@ -181,6 +181,28 @@ async function getUpcomingList() {
 
     displayUpcomingList(data.results);
 
+    // MODAL QUICK VIEW
+    let previewContainer = document.querySelector("#upcomingList-cards");
+    let proviewBox = previewContainer.querySelectorAll(".modal");
+    // SHOW MODAL
+    document.querySelectorAll("#upcomingList-cards .cards-link").forEach(button => {
+      button.onclick = () => {
+        let name = button.getAttribute("data-name");
+        proviewBox.forEach(modal => {
+          let target = modal.getAttribute("data-target");
+          if(name == target) {
+            modal.classList.add("active");
+          }
+        })
+      }
+    })
+    // CLOSE MODAL
+    proviewBox.forEach(close => {
+      close.querySelector(".modal-close").onclick = () => {
+        close.classList.remove("active");
+      }
+    })
+
 }
 
 const displayUpcomingList = (upcomingList) => {
@@ -215,7 +237,91 @@ const displayUpcomingList = (upcomingList) => {
 
         link.appendChild(card);
 
+        // MODAL MAIN MAIN
+        let cardMain = document.createElement("section");
+        cardMain.classList.add("modal");
+        cardMain.setAttribute("data-target", movie.id)
+        // cards 2 MAIN container
+        let card2Main = document.createElement("section");
+        card2Main.classList.add("modal-1");
+        // cards container
+        let card2 = document.createElement("section");
+        card2.classList.add("modal-container");
+        // img and overlay container
+        let imgOverlayContainer = document.createElement("div");
+        imgOverlayContainer.classList.add("modal-imgOverlayContainer");
+        // img
+        let img2 = document.createElement("img");
+        img2.setAttribute("src", `https://image.tmdb.org/t/p/original${movie.poster_path}`);
+        // overlay
+        let overlay2 = document.createElement("div");
+        overlay2.classList.add("modal-overlay");
+        // overlay title
+        let title2 = document.createElement("h2");
+        title2.classList.add("modal-title");
+        title2.innerHTML = `${movie.original_title}`;
+
+        // rating and genres container
+        let rateGenresContainer = document.createElement("div");
+        rateGenresContainer.classList.add("modal-rating-genres-container");
+        // rate and date container
+        let rateDateContainer = document.createElement("div");
+        rateDateContainer.classList.add("modal-rating-vote-container");
+        // rating container
+        let ratingContainer = document.createElement("div");
+        ratingContainer.classList.add("modal-rating-container");
+        // rating
+        let rating = document.createElement("span");
+        rating.classList.add("modal-rating");
+        rating.innerHTML = `${movie.vote_average}`;
+        // rating total
+        let ratingTotal = document.createElement("span");
+        ratingTotal.classList.add("modal-rating-total");
+        ratingTotal.innerHTML = "/10";
+        // rating total
+        let voteCount = document.createElement("span");
+        voteCount.classList.add("modal-time");
+        voteCount.innerHTML = `${movie.vote_count} Votes`;
+        // date
+        let date = document.createElement("p");
+        date.classList.add("modal-date");
+        date.innerHTML = `${movie.release_date}`;
+        // overview
+        let overview = document.createElement("p");
+        overview.classList.add("modal-description");
+        overview.innerHTML = movie.overview;
+
+        // close button
+        let closeButton = document.createElement("button");
+        closeButton.classList.add("modal-close");
+        closeButton.innerHTML = "X";
+
+
+        ratingContainer.appendChild(rating);
+        ratingContainer.appendChild(ratingTotal);
+
+        rateDateContainer.appendChild(ratingContainer);
+        rateDateContainer.appendChild(voteCount);
+
+        rateGenresContainer.appendChild(rateDateContainer);
+
+        overlay2.appendChild(title2);
+        overlay2.appendChild(rateGenresContainer);
+        overlay2.appendChild(date);
+
+        imgOverlayContainer.appendChild(img2);
+        imgOverlayContainer.appendChild(overlay2);
+
+        card2.appendChild(imgOverlayContainer);
+        card2.appendChild(overview);
+
+        card2Main.appendChild(card2);
+        card2Main.appendChild(closeButton);
+
+        cardMain.appendChild(card2Main);
+
         upcomingListContainer.appendChild(link);
+        upcomingListContainer.appendChild(cardMain);
 
         
 
@@ -241,6 +347,28 @@ async function getRatedList() {
     // console.log(data.results); // testing
 
     displayRatedList(data.results);
+
+    // MODAL QUICK VIEW
+    let previewContainer = document.querySelector("#ratedList-cards");
+    let proviewBox = previewContainer.querySelectorAll(".modal");
+    // SHOW MODAL
+    document.querySelectorAll("#ratedList-cards .cards-link").forEach(button => {
+      button.onclick = () => {
+        let name = button.getAttribute("data-name");
+        proviewBox.forEach(modal => {
+          let target = modal.getAttribute("data-target");
+          if(name == target) {
+            modal.classList.add("active");
+          }
+        })
+      }
+    })
+    // CLOSE MODAL
+    proviewBox.forEach(close => {
+      close.querySelector(".modal-close").onclick = () => {
+        close.classList.remove("active");
+      }
+    })
 
 }
 
@@ -278,7 +406,91 @@ const displayRatedList = (ratedList) => {
 
 
 
+        // MODAL MAIN MAIN
+        let cardMain = document.createElement("section");
+        cardMain.classList.add("modal");
+        cardMain.setAttribute("data-target", movie.id)
+        // cards 2 MAIN container
+        let card2Main = document.createElement("section");
+        card2Main.classList.add("modal-1");
+        // cards container
+        let card2 = document.createElement("section");
+        card2.classList.add("modal-container");
+        // img and overlay container
+        let imgOverlayContainer = document.createElement("div");
+        imgOverlayContainer.classList.add("modal-imgOverlayContainer");
+        // img
+        let img2 = document.createElement("img");
+        img2.setAttribute("src", `https://image.tmdb.org/t/p/original${movie.poster_path}`);
+        // overlay
+        let overlay2 = document.createElement("div");
+        overlay2.classList.add("modal-overlay");
+        // overlay title
+        let title2 = document.createElement("h2");
+        title2.classList.add("modal-title");
+        title2.innerHTML = `${movie.original_title}`;
+
+        // rating and genres container
+        let rateGenresContainer = document.createElement("div");
+        rateGenresContainer.classList.add("modal-rating-genres-container");
+        // rate and date container
+        let rateDateContainer = document.createElement("div");
+        rateDateContainer.classList.add("modal-rating-vote-container");
+        // rating container
+        let ratingContainer = document.createElement("div");
+        ratingContainer.classList.add("modal-rating-container");
+        // rating
+        let rating = document.createElement("span");
+        rating.classList.add("modal-rating");
+        rating.innerHTML = `${movie.vote_average}`;
+        // rating total
+        let ratingTotal = document.createElement("span");
+        ratingTotal.classList.add("modal-rating-total");
+        ratingTotal.innerHTML = "/10";
+        // rating total
+        let voteCount = document.createElement("span");
+        voteCount.classList.add("modal-time");
+        voteCount.innerHTML = `${movie.vote_count} Votes`;
+        // date
+        let date = document.createElement("p");
+        date.classList.add("modal-date");
+        date.innerHTML = `${movie.release_date}`;
+        // overview
+        let overview = document.createElement("p");
+        overview.classList.add("modal-description");
+        overview.innerHTML = movie.overview;
+
+        // close button
+        let closeButton = document.createElement("button");
+        closeButton.classList.add("modal-close");
+        closeButton.innerHTML = "X";
+
+
+        ratingContainer.appendChild(rating);
+        ratingContainer.appendChild(ratingTotal);
+
+        rateDateContainer.appendChild(ratingContainer);
+        rateDateContainer.appendChild(voteCount);
+
+        rateGenresContainer.appendChild(rateDateContainer);
+
+        overlay2.appendChild(title2);
+        overlay2.appendChild(rateGenresContainer);
+        overlay2.appendChild(date);
+
+        imgOverlayContainer.appendChild(img2);
+        imgOverlayContainer.appendChild(overlay2);
+
+        card2.appendChild(imgOverlayContainer);
+        card2.appendChild(overview);
+
+        card2Main.appendChild(card2);
+        card2Main.appendChild(closeButton);
+
+        cardMain.appendChild(card2Main);
+
         ratedListContainer.appendChild(link);
+        ratedListContainer.appendChild(cardMain);
 
         
 
